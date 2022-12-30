ce16b816ad79eb2ce5f08189260abc9042b4a606
이재원 <jenyglee_30@naver.com>
2022-12-30 11:18:09 +0900

    [feat] post 엔티티 설계 진행중

--- src/main/java/sparta/bus10/entity/Post.java
+++ src/main/java/sparta/bus10/entity/Post.java
@@ -2,6 +2,7 @@ package sparta.bus10.entity;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,4 +13,9 @@ public class Post {
     @Id
     @GeneratedValue
     private Long postId;
+
+    @Column(nullable = false)
+    private String postTitle;
+    @Column(nullable = false)
+    private String postContent;
 }
