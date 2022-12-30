6c1eec2787c88c8eac5cd00efa7d1369ec94a9a6
Gwanho KIM <bigquan977@gmail.com>
2022-12-30 11:16:30 +0900

    [fix] gitignore 수정 후 commit

---
+++ src/test/java/sparta/bus10/Bus10ApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.bus10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bus10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
