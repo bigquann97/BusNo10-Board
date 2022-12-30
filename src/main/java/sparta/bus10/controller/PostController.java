7277cf1a84bd139f1982a59413f5330f1ce38460
이재원 <jenyglee_30@naver.com>
2022-12-30 11:54:31 +0900

    [feat] PostController 수정

--- src/main/java/sparta/bus10/controller/PostController.java
+++ src/main/java/sparta/bus10/controller/PostController.java
@@ -1,4 +1,16 @@
 package sparta.bus10.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sparta.bus10.service.PostService;
+
+@RestController
+@RequestMapping("/api")
 public class PostController {
+    private final PostService postService;
+    @PostMapping("/post")
+    public void createPost(){
+
+    }
 }
