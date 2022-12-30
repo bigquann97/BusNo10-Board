f0feb314a5a98abe422cf2ccd360adf32c092292
antidote <ljh8651@nate.com>
2022-12-30 16:34:53 +0900

    [feat] 게시글 수정 , 삭제 기능 + orElseThrow 추가

--- src/main/java/sparta/bus10/repository/PostRepository.java
+++ src/main/java/sparta/bus10/repository/PostRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sparta.bus10.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post findByPostId(Long id);
 }
