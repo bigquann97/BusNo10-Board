a55744011ab6016c572acf24a2faf495bb61f3ed
Gwanho KIM <bigquan977@gmail.com>
2023-01-01 18:37:07 +0900

    [refactor] service, repository 가독성 추가

--- src/main/java/sparta/bus10/service/PostService.java
+++ src/main/java/sparta/bus10/service/PostService.java
@@ -7,7 +7,6 @@ import sparta.bus10.dto.PostRequestDto;
 import sparta.bus10.dto.PostResponseDto;
 import sparta.bus10.entity.Comment;
 import sparta.bus10.entity.Post;
-import sparta.bus10.entity.UserRoleEnum;
 import sparta.bus10.repository.CommentRepository;
 import sparta.bus10.repository.PostRepository;
 
@@ -17,16 +16,17 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
     @Transactional
     public void createPost(PostRequestDto postrequestDto) {
-        postRepository.save(new Post(postrequestDto.getUserName(),postrequestDto.getPostTitle(),postrequestDto.getPostContent()));
+        postRepository.save(new Post(postrequestDto.getUserName(), postrequestDto.getPostTitle(), postrequestDto.getPostContent()));
     }
 
     @Transactional
-    public List<PostResponseDto> getPostAll(){
+    public List<PostResponseDto> getPostAll() {
         List<Post> posts = postRepository.findAll();
         List<PostResponseDto> resultPosts = new ArrayList<>();
         for (Post post : posts) {
@@ -39,34 +39,35 @@ public class PostService {
     }
 
     @Transactional
-    public PostResponseDto getPostOne(Long postId){
+    public PostResponseDto getPostOne(Long postId) {
         Post post = postRepository.findById(postId).orElseThrow(
-            ()-> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
         List<Comment> comment = commentRepository.findByPostId(postId);
-        PostResponseDto postResponseDto = new PostResponseDto(post,comment);
+        PostResponseDto postResponseDto = new PostResponseDto(post, comment);
         return postResponseDto;
     }
 
     @Transactional
-    public void editPost(Long postId , PostRequestDto postrequestDto) {
-       Post post = postRepository.findById(postId).orElseThrow(
-               ()-> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
-       );
-        if (!post.getUsername().equals(postrequestDto.getUserName())){
+    public void editPost(Long postId, PostRequestDto postrequestDto) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
+        );
+        if (!post.getUsername().equals(postrequestDto.getUserName())) {
             throw new IllegalArgumentException("유저의 이름이 일치하지 않습니다.");
         }
-       post.changePost(postrequestDto.getUserName(),postrequestDto.getPostTitle(),postrequestDto.getPostContent());
-       postRepository.save(post);
+        post.changePost(postrequestDto.getUserName(), postrequestDto.getPostTitle(), postrequestDto.getPostContent());
+        postRepository.save(post);
     }
 
     @Transactional
-    public void deletePost(Long postId, PostRequestDto postrequestDto){
+    public void deletePost(Long postId, PostRequestDto postrequestDto) {
         Post post = postRepository.findById(postId).orElseThrow(
-                ()-> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
+                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
         );
-        if (!post.getUsername().equals(postrequestDto.getUserName())){
+        if (!post.getUsername().equals(postrequestDto.getUserName())) {
             throw new IllegalArgumentException("유저의 이름이 일치하지 않습니다.");
         }
         postRepository.delete(post);
     }
+
 }
