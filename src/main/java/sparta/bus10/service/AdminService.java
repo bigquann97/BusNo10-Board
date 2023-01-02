537ec171e122c5af6fb4af92e621cefdc04195e2
SONGMI <d.cheese1113@gmail.com>
2023-01-02 14:42:08 +0900

    [fix] Admin Conflict 해결

--- src/main/java/sparta/bus10/service/AdminService.java
+++ src/main/java/sparta/bus10/service/AdminService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sparta.bus10.dto.CommentRequestDto;
+import sparta.bus10.dto.PostRequestDto;
+import sparta.bus10.entity.Comment;
 import sparta.bus10.entity.Post;
 import sparta.bus10.repository.CommentRepository;
 import sparta.bus10.repository.PostRepository;
@@ -15,11 +18,48 @@ public class AdminService {
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
+
+    // 어드민 권한 게시글 수정
+    @Transactional
+    public void editPostByAdmin(Long postId, PostRequestDto postRequestDto) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
+        );
+        post.changePost(postRequestDto.getUserName(), postRequestDto.getPostTitle(), postRequestDto.getPostContent());
+        postRepository.save(post);
+    }
+
+    // 어드민 권한 게시글 삭제
     @Transactional
     public void deletePostByAdmin(Long postId) {
         Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 게시물 없음"));
         postRepository.delete(post);
     }
 
-    // 메서드 3개 더 필요
+    // 어드민 권한 게시글 수정
+    @Transactional
+    public void editCommentByAdmin(Long postId, Long commentId, CommentRequestDto commentRequestDto){
+        postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
+        );
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new IllegalArgumentException("댓글을 찾을 수 없습니다.")
+        );
+        comment.changeComment(commentRequestDto.getCommentContent());
+        commentRepository.save(comment);
+    }
+
+    // 어드민 권한 댓글 삭제
+    @Transactional
+    public void deleteCommentByAdmin(Long postId, Long commentId) {
+        postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
+        );
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new IllegalArgumentException("댓글을 찾을 수 없습니다.")
+        );
+        commentRepository.delete(comment);
+    }
+
+
 }
