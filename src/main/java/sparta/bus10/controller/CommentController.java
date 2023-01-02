00dfea57164694668203f6c2f828a70bca6a5891
Gwanho KIM <bigquan977@gmail.com>
2023-01-02 13:13:21 +0900

    [refactor] 댓글 Security 권한 적용

--- src/main/java/sparta/bus10/controller/CommentController.java
+++ src/main/java/sparta/bus10/controller/CommentController.java
@@ -2,8 +2,10 @@ package sparta.bus10.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sparta.bus10.dto.CommentRequestDto;
+import sparta.bus10.security.UserDetailsImpl;
 import sparta.bus10.service.CommentService;
 
 @RestController
@@ -14,18 +16,33 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/posts/{postId}/comments")
-    public void createCommentService(@PathVariable Long postId, @RequestBody CommentRequestDto requestDto) {
-        commentService.createCommentService(postId, requestDto);
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public void createCommentService(
+            @PathVariable Long postId,
+            @RequestBody CommentRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        commentService.createCommentService(postId, requestDto, userDetails.getUser());
     }
 
     @PutMapping("/posts/{postId}/comments/{commentId}")
-    public void editComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody CommentRequestDto commentrequestDto) {
-        commentService.editComment(postId, commentId, commentrequestDto);
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public void editComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @RequestBody CommentRequestDto commentrequestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        commentService.editComment(postId, commentId, commentrequestDto, userDetails.getUser());
     }
 
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
-    public void deleteComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody String userName) {
-        commentService.deleteComment(postId, commentId, userName);
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public void deleteComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        commentService.deleteComment(postId, commentId, userDetails.getUser());
     }
 
 }
--- src/main/java/sparta/bus10/service/CommentService.java
+++ src/main/java/sparta/bus10/service/CommentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sparta.bus10.dto.CommentRequestDto;
 import sparta.bus10.entity.Comment;
+import sparta.bus10.entity.User;
 import sparta.bus10.repository.CommentRepository;
 import sparta.bus10.repository.PostRepository;
 
@@ -16,38 +17,40 @@ public class CommentService {
     private final PostRepository postRepository;
 
     @Transactional
-    public void createCommentService(Long postId, CommentRequestDto commentrequestDto) {
+    public void createCommentService(Long postId, CommentRequestDto commentrequestDto, User user) {
         postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
         );
-        Comment comment = new Comment(postId, commentrequestDto.getUserName(), commentrequestDto.getCommentContent());
+        Comment comment = new Comment(postId, user.getUsername(), commentrequestDto.getCommentContent());
         commentRepository.save(comment);
     }
 
     @Transactional
-    public void editComment(Long postId, Long commentId, CommentRequestDto commentRequestDto) {
+    public void editComment(Long postId, Long commentId, CommentRequestDto commentRequestDto, User user) {
         postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
         );
-        Comment comment = commentRepository.findById(commentId).orElseThrow(
-                () -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
+//        Comment comment = commentRepository.findById(commentId).orElseThrow(
+//                () -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
         //유효성검사 이름 비교
-        if (!comment.getUsername().equals(commentRequestDto.getUserName())) {
-            throw new IllegalArgumentException("유저의 이름이 일치하지 않습니다.");
-        }
+//        if (!comment.getUsername().equals(commentRequestDto.getUserName())) {
+//            throw new IllegalArgumentException("유저의 이름이 일치하지 않습니다.");
+//        }
+        Comment comment = commentRepository.findByCommentIdAndUsername(commentId, user.getUsername()).orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
         comment.changeComment(commentRequestDto.getCommentContent());
         commentRepository.save(comment);
     }
 
     @Transactional
-    public void deleteComment(Long postId, Long commentId, String userName) {
+    public void deleteComment(Long postId, Long commentId, User user) {
         postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("게시글을 찾을 수 없습니다.")
         );
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
-        if (!comment.getUsername().equals(userName)) {
-            throw new IllegalArgumentException("유저의 이름이 일치하지 않습니다.");
-        }
+//        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
+//        if (!comment.getUsername().equals(userName)) {
+//            throw new IllegalArgumentException("유저의 이름이 일치하지 않습니다.");
+//        }
+        Comment comment = commentRepository.findByCommentIdAndUsername(commentId, user.getUsername()).orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
         commentRepository.delete(comment);
     }
 
