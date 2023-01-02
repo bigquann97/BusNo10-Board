87e3d3dfd7b2150e9605a56dd89d9ffb2c2b90d2
Gwanho KIM <bigquan977@gmail.com>
2023-01-02 16:15:34 +0900

    [refactor] 가독성 추가, 접근 권한 공통 부분 묶기

--- src/main/java/sparta/bus10/controller/AdminController.java
+++ src/main/java/sparta/bus10/controller/AdminController.java
@@ -11,38 +11,30 @@ import sparta.bus10.service.AdminService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/admin") // root
+@RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     private final AdminService adminService;
 
-    // http://localhost:8080/api/admin/posts/1 - Header Token값을 넣어서 보내줘요 Authorization : Bearer aivbi3
-    // 어드민 권한 게시글 삭제
-    @DeleteMapping ("/posts/{postId}") // Spring Annocation
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping ("/posts/{postId}")
     public void deletePostByAdmin(@PathVariable Long postId) {
         adminService.deletePostByAdmin(postId);
     }
 
-    // 어드민 권한 게시글 수정 - editPostByAdmin
-    @PutMapping("/posts/{postId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PatchMapping("/posts/{postId}")
     public void editPostByAdmin(@PathVariable Long postId, @RequestBody PostRequestDto postRequestDto){
         adminService.editPostByAdmin(postId, postRequestDto);
     }
 
-    // 어드민 권한 댓글 삭제 - deleteCommentByAdmin
-    @DeleteMapping("/posts/{postId}/comments/{commentId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public void deleteCommentByAdmin(@PathVariable Long postId, @PathVariable Long commentId) {
-        adminService.deleteCommentByAdmin(postId, commentId);
+    @DeleteMapping("/comments/{commentId}")
+    public void deleteCommentByAdmin(@PathVariable Long commentId) {
+        adminService.deleteCommentByAdmin(commentId);
     }
 
-    // 어드민 권한 댓글 수정 - editCommentByAdmin
-    @PutMapping("/posts/{postId}/comments/{commentId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public void editCommentByAdmin(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody CommentRequestDto commentRequestDto) {
-        adminService.editCommentByAdmin(postId, commentId, commentRequestDto);
+    @PatchMapping("/comments/{commentId}")
+    public void editCommentByAdmin(@PathVariable Long commentId, @RequestBody CommentRequestDto commentRequestDto) {
+        adminService.editCommentByAdmin(commentId, commentRequestDto);
     }
 
 }
