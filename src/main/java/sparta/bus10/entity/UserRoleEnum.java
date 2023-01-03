525a0c23957685f7e0851c999205995904cb84ee
이재원 <jenyglee_30@naver.com>
2023-01-03 12:59:40 +0900

    Revert "[feat] 매니저(MANAGER) 권한 추가"
    This reverts commit e30dab0cbc6d7cfef4a42ce214883aaf54bb08bb.

--- src/main/java/sparta/bus10/entity/UserRoleEnum.java
+++ src/main/java/sparta/bus10/entity/UserRoleEnum.java
@@ -2,9 +2,7 @@ package sparta.bus10.entity;
 
 public enum UserRoleEnum {
     USER(Authority.USER),  // 사용자 권한
-    MANAGER(Authority.MANAGER), // 매니저 권한
     ADMIN(Authority.ADMIN);  // 관리자 권한
-
 
     private final String authority;
 
@@ -20,6 +18,5 @@ public enum UserRoleEnum {
     public static class Authority{
         public static final String USER = "ROLE_USER";
         public static final String ADMIN = "ROLE_ADMIN";
-        public static final String MANAGER = "ROLE_MANAGER";
     }
 }
